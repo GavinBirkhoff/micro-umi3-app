d446a2b77cc0d8e8aeca3a92da72860438edeb01
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-06-06 22:09:49 +0800

    Add user-app login

---
+++ other-app/src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>
+        子应用不建议独立登录，主应用中心化登录鉴权。如果有特殊业务需求可以在这里写登录。
+      </h1>
+    </div>
+  );
+};
+
+export default LoginPage;
--- user-app/src/app.ts
+++ user-app/src/app.ts
@@ -1,3 +1,40 @@
+import { useState } from 'react';
+import { history, RequestConfig } from 'umi';
+import store from 'local-store-pro';
+
+const isDev = process.env.NODE_ENV === 'development';
+const loginPath = '/login';
+
+/**
+ * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
+ * */
+export async function getInitialState(): Promise<{
+  info?:any
+  fetchInfo?: () => Promise<any | undefined>;
+}> {
+  const fetchInfo = async () => {
+    try {
+      if (!store('token')) {
+        throw Error('has no token');
+      }
+    } catch (error) {
+      history.push(loginPath);
+    }
+    return undefined;
+  };
+  console.log(history.location);
+  // 如果不是登录页面，执行
+  if (history.location.pathname !== loginPath) {
+    const info = await fetchInfo();
+    return {
+      info,
+      fetchInfo,
+    };
+  }
+  return {
+  };
+}
+
 export const qiankun = {
   // 应用加载之前
   async bootstrap(props: any) {
